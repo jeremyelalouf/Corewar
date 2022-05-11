f0de11a761c15e10939dca7f8b1f673e0f78aabc
jerem <jeremy.elalouf@epitech.eu>
2022-05-11 14:42:32 +0200

    feat: op.h coding style

--- includes/op.h
+++ includes/op.h
@@ -7,29 +7,21 @@
 
 #ifndef _OP_H_
    #define _OP_H_
-
    #define MEM_SIZE (6 * 1024)
    #define MAX_ARGS_NUMBER 4
    #define IDX_MOD 512
-
    #define SEPARATOR_CHAR ','
    #define COMMENT_CHAR '#'
    #define DIRECT_CHAR '%'
    #define LABEL_CHAR ':'
-
    #define LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
-
    #define NAME_CMD_STRING ".name"
    #define COMMENT_CMD_STRING ".comment"
-
    #define REG_NUMBER 16
-
-   #define T_REG 1       /* register */
-   #define T_DIR 2       /* direct  (ld  #1,r1  put 1 into r1) */
-   #define T_IND 4       /* indirect always relative
-                                    ( ld 1,r1 put what's in the address (1+pc)
-                                    into r1 (4 bytes )) */
-   #define T_LAB 8       /* LABEL */
+   #define T_REG 1
+   #define T_DIR 2
+   #define T_IND 4
+   #define T_LAB 8
 
 typedef char args_type_t;
 
@@ -42,6 +34,7 @@ typedef struct op_s {
    char *comment;
 } op_t;
 
+   // in byte
    #define IND_SIZE        2
    #define DIR_SIZE        4
    #define REG_SIZE        DIR_SIZE
@@ -53,13 +46,13 @@ extern  op_t    op_tab[];
 
 typedef struct header_s {
    int  magic;
-   #define COREWAR_EXEC_MAGIC      0xea83f3        /* why not */
+   #define COREWAR_EXEC_MAGIC      0xea83f3
    char prog_name[PROG_NAME_LENGTH + 1];
    int  prog_size;
    char comment[COMMENT_LENGTH + 1];
 } header_t;
 
-   #define CYCLE_TO_DIE    1536    /* number of cycle before beig declared dead */
+   #define CYCLE_TO_DIE    1536
    #define CYCLE_DELTA     5
    #define NBR_LIVE        40
 
